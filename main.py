0b6b22f42112221347dbff98e0478b2944db180f
Landon <landoncramer1@gmail.com>
2025-06-26 20:19:14 -0700

    take orders

--- main.py
+++ main.py
@@ -10,16 +10,26 @@ def show_menu():
     print("5. Don Cali Burger - $5.95")
     print("6. Exit")
     
-def main():
+def get_valid_input(prompt, min, max):
+    pass
 
-    menue = {
+def take_orders(prices):
+    """Presents menue return dic mapping item number to quantity ordered"""
+    quantities = {i:0 for i in prices}
+    while True:
+        show_menu()
+        choice = get_valid_input("Enter your choice (1-6): ", 1, 6)
+        
+    
+def main():
+    menu = {
         1: "De Anza Burger", 
         2: "Bacon Cheese",
         3: "Mushroom Swiss",
         4: "Western Burger",
         5: "Don Cali Burger"
     }
-    price = {
+    prices = {
         1: 5.25,
         2: 5.75,
         3: 5.95,
@@ -27,5 +37,7 @@ def main():
         5: 5.95
     }
         
-    quantities = take_orders(price):
-        pass
+    quantities = take_orders(prices)
+
+if __name__ == "__main__":
+    main()
