68698fe6be0506c502dc80418b6993435755202c
Landon <landoncramer1@gmail.com>
2025-06-26 17:05:04 -0700

    first

---
+++ main.py
@@ -0,0 +1,23 @@
+
+def show_menu():
+    print("--- De Anza Food Cou:
+          pass 
+def main():
+
+    menue = {
+        1: "De Anza Burger", 
+        2: "Bacon Cheese",
+        3: "Mushroom Swiss",
+        4: "Western Burger",
+        5: "Don Cali Burger"
+    }
+    price = {
+        1: 5.25,
+        2: 5.75,
+        3: 5.95,
+        4: 5.95,
+        5: 5.95
+    }
+        
+    quantities = take_orders(price):
+        pass
